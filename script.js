8a368b0819c1dc46689eb1b0c93193497788aeb9
lebronhawktuah <kamilthe217@gmail.com>
2026-05-06 13:23:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const URL = 'https://yibxzgvirmkryqurnvxq.supabase.co';
+const KEY = 'sb_publishable_S79b2G7PSbbBJRUaKI5b8g_quXdiz2S';
+const sb = supabase.createClient(URL, KEY);
 
+// The rest of your code follows...
